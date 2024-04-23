b13e1020b61022f70f60b16ad2d23424fd971d4f
LIAhbm <138190311+LIAhbm@users.noreply.github.com>
2024-04-22 23:12:49 -0600

    Add files via upload

---
+++ PROYECTO1/protyecto1/src/protyecto1/Protyecto1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package protyecto1;
+
+/**
+ *
+ * @author pc
+ */
+public class Protyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jfprincipal ir = new jfprincipal();
+        ir.setVisible(true);
+        
+    }
+    
+}
